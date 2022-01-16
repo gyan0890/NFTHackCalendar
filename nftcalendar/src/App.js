b7cb34efda24c14f6e98fe13440747a55f5167e5
Abhishek More <abhishek.more@energyexemplar.com>
2022-01-16 19:47:30 +0530

    add tabs

--- nftcalendar/src/App.js
+++ nftcalendar/src/App.js
@@ -4,7 +4,7 @@ import { useContext, useEffect } from 'react';
 import WalletService from './services/wallet';
 import { AppContext, AppContextProvider } from './states/appcontext';
 import { AppActions } from './states/actions';
-
+import Tabs from "./components/Tabs";
 function App() {
 
   const [state, dispatch] = useContext(AppContext);
@@ -23,9 +23,18 @@ function App() {
 
   }, []);
   return (
-
     <div className="App">
-      <Calendar />
+      <Tabs>
+        <div label="Transact">
+         <Calendar/>
+        </div>
+        <div label="My NFT">
+          After 'while, <em>Crocodile</em>!
+        </div>
+        <div label="Profile">
+          Nothing to see here, this tab is <em>extinct</em>!
+        </div>
+      </Tabs>
     </div>
 
   );
