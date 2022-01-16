2c4f4cb6dbfc88b38d07ba479e86917460757f1e
vikaspachori <vikas_pachori@yahoo.com>
2022-01-16 14:24:32 +0530

    added pinata load

--- nftcalendar/src/services/pinataservice.js
+++ nftcalendar/src/services/pinataservice.js
@@ -5,7 +5,8 @@ export default class PinataService {
     static url = "https://api.pinata.cloud/data/pinList?pageLimit=365";
 
     static async getPinataStorage() {
-       
+        const apikey = 'dc5687c5d5430bf00519';
+        const secret = '3cd065362925cb71194af28112954f28562421dec986a66a6c1dcb5c7fe3f576';
         const headers = {
             'accept': 'text/plain',
             'Content-Type': 'application/json',
